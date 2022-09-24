59a891a1acef61eb8982b7d0d19a7d8bd299ee8f
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2022-09-24 17:51:20 +0300

    rename pr2 to pr1

--- pr1/src/pr2/t2/Main.java
+++ pr1/src/pr2/t2/Main.java
@@ -8,8 +8,8 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class Main {
-    private static int MIN_DELAY = 1000;
-    private static int MAX_DELAY = 5000;
+    private static final int MIN_DELAY = 1000;
+    private static final int MAX_DELAY = 5000;
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
@@ -17,25 +17,31 @@ public class Main {
         ArrayList<FutureTask<String>> tasks = new ArrayList<>();
 
         while (true) {
-            callbackFutureTasksIfDone(tasks);
-
             if (System.in.available() != 0 && scanner.hasNextInt()) {
                 int number = scanner.nextInt();
                 FutureTask<String> futureTask = new FutureTask<>(createCallable(number));
 
                 tasks.add(futureTask);
                 new Thread(futureTask).start();
+            } else {
+                callbackFutureTasksIfDone(tasks);
             }
         }
     }
 
     private static void callbackFutureTasksIfDone (ArrayList<FutureTask<String>> tasks) throws ExecutionException, InterruptedException {
-        for (int i = tasks.size() - 1; i >= 0; i--) {
-            if (tasks.get(i).isDone()) {
-                FutureTask<String> futureTask = tasks.remove(i);
+//        for (int i = tasks.size() - 1; i >= 0; i--) {
+//            if (tasks.get(i).isDone()) {
+//                FutureTask<String> futureTask = tasks.remove(i);
+//
+//                System.out.println(futureTask.get());
+//            }
+//        }
 
-                System.out.println(futureTask.get());
-            }
+        if (!tasks.isEmpty() && tasks.get(0).isDone()) {
+            FutureTask<String> futureTask = tasks.remove(0);
+
+            System.out.println(futureTask.get());
         }
     }
 
