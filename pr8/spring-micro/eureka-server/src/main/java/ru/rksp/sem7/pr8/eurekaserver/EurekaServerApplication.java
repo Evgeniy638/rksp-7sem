2b48ce8dadb04e24042cabb2f503d10e48d0a90b
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2022-12-17 13:24:23 +0300

    init spring-micro

---
+++ pr8/spring-micro/eureka-server/src/main/java/ru/rksp/sem7/pr8/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.sem7.pr8.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ pr8/spring-micro/eureka-server/src/test/java/ru/rksp/sem7/pr8/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.sem7.pr8.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
