c535277a5dc5fcb4f255f3928b020758fa592986
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2022-12-17 15:45:02 +0300

    add api gateway

---
+++ pr8/spring-micro/api-gateway/src/test/java/ru/rksp/sem7/pr8/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.sem7.pr8.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
